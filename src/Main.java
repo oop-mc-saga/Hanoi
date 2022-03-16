b950d9d58b31b6d0a31509cb9cdcb09e02899c64
tada195908 <48470775+tada195908@users.noreply.github.com>
2022-03-16 09:13:41 +0900

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import model.*;
+/**
+ *
+ * @author tadaki
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Hanoi hanoi = new Hanoi(3);
+        hanoi.start();
+    }
+    
+}
