74e297d12adcb9eb95410eafa844c7bb0c1e8fa1
tada195908 <48470775+tada195908@users.noreply.github.com>
2022-03-16 09:13:56 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Hanoi hanoi = new Hanoi(3);
-        hanoi.start();
+
+    
     }
     
 }
