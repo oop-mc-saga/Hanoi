b6b42c1db633fc1dea6baaa4a86c4b8537f13e64
tada195908 <48470775+tada195908@users.noreply.github.com>
2022-06-27 10:07:57 +0900

    Update Disk.java

--- src/parts/Disk.java
+++ src/parts/Disk.java
@@ -19,7 +19,8 @@ public class Disk implements Comparable<Disk>{
    
     @Override
     public String toString(){
-        return String.valueOf(n);
+        
+        return "something";
     }
     
 }
